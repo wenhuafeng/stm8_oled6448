2877f995a098645783377e3fd064869d05e3af9e
wenhuafeng <547566993@qq.com>
2022-10-27 23:46:18 +0800

    加入STVP_CmdLine.exe烧录芯片支持

--- build.py
+++ build.py
@@ -8,21 +8,26 @@ import subprocess
 import shutil
 from subprocess import Popen, PIPE
 
-CHECKSUM_OUT = 'checksum.exe sc1086.bin CRC32 SHA256'
+CHECKSUM_OUT = "checksum.exe sc1086.bin CRC32"
 
 # IAR path
 iar_build_exe = "D:/Program Files (x86)/IAR Systems/Embedded Workbench 8.0/common/bin/IarBuild.exe"
-iar_build_log = 'cat ./project/build_log.txt'
+iar_build_log = "cat ./project/build_log.txt"
 
 # output files path
-iar_source_file_hex = 'project/Debug/Exe/sc1086.hex'
-iar_source_file_bin = 'project/Debug/Exe/sc1086.bin'
-target_path = 'user/output'
+iar_source_file_hex = "project/Debug/Exe/sc1086.hex"
+iar_source_file_bin = "project/Debug/Exe/sc1086.bin"
+target_path = "user/output"
 
 # openocd define
 stlink_config_file = "user/openocd/stlink-dap.cfg"
 chip_config_file = "user/openocd/stm8l.cfg"
-program_cmd = "\"program user/output/sc1086.bin 0x8000 verify reset exit\""
+program_cmd = "\"init; load_image user/output/sc1086.bin 0x8000; verify; reset; shutdown\""
+
+# STVP_CmdLine.exe define
+stvp_cmd_Line           = "D:/Program Files (x86)/STMicroelectronics/st_toolset/stvp/STVP_CmdLine.exe"
+stvp_cmd_Line_rm_log    = "rm ./Result.log"
+stvp_cmd_Line_print_log = "cat ./Result.log"
 
 def get_exitcode_stdout_stderr(args):
     """
@@ -45,7 +50,7 @@ def cp_build_file(source, target):
 
 def iar_build():
     print("cc type = iar")
-    exitcode, out, err = get_exitcode_stdout_stderr([iar_build_exe, "project/sc1086.ewp", "-build", "Debug", "-log", "info", ">", "project/build_log.txt"])
+    get_exitcode_stdout_stderr([iar_build_exe, "project/sc1086.ewp", "-build", "Debug", "-log", "info", ">", "project/build_log.txt"])
     os.system(iar_build_log)
     cp_build_file(iar_source_file_bin, target_path)
 
@@ -57,16 +62,14 @@ def stlink_download():
     print("host os: %s" % host_os)
 
     if host_os == 'Windows':
-        openocd_exe = "openocd.exe"
-    elif host_os == 'Linux':
-        openocd_exe = "openocd"
+        #cmd = 'STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Tool_ID=0 -Device=STM8L051x3 -log -verbose -progress -no_loop -FileProg=./user/output/sc1086.hex'
+        #subprocess.Popen(cmd)
+        os.system(stvp_cmd_Line_rm_log)
+        get_exitcode_stdout_stderr([stvp_cmd_Line, "-BoardName=ST-LINK", "-ProgMode=SWIM", "-Tool_ID=0", "-Device=STM8L051x3", "-log", "-verbose", "-progress", "-no_loop", "-FileProg=./user/output/sc1086.hex"])
+        os.system(stvp_cmd_Line_print_log)
     else:
         print("unsupport platform")
         sys.exit(2)
-
-    exec_cmd = "%s -f %s -f %s -c %s\n" % (openocd_exe, stlink_config_file, chip_config_file, program_cmd)
-    print("execute cmd: %s" % exec_cmd)
-    subprocess.call(exec_cmd, shell=True)
 
 def build_select(para):
     if para == 'iar':
